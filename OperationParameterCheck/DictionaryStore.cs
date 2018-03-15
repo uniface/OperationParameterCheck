bf78e106eb02934ce35d450737e65c8bec80e905
thomassshore <thomas.shore@uniface.com>
2018-03-15 14:32:46 -0400

    Add files via upload
    Initial population

---
+++ OperationParameterCheck/DictionaryStore.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationParameterCheck
+{
+    class DictionaryStore
+    {
+        private string operationName;
+        private int correctParamNum;
+        private List<OperationCall> callList;
+
+        public DictionaryStore(string optName, int numParams)
+        {
+            operationName = optName;
+            correctParamNum = numParams;
+            callList = new List<OperationCall>();
+        }
+
+        public void IncrementParamsCount()
+        {
+            this.correctParamNum++;
+        }
+
+        public void AddCall(OperationCall call)
+        {
+            this.callList.Add(call);
+        }
+
+        public List<OperationCall> GetCallList()
+        {
+            return callList;
+        }
+
+        public int GetCorrectParamCount()
+        {
+            return correctParamNum;
+        }
+
+        public void SetParamCount()
+        {
+            correctParamNum = 0;
+        }
+    }
+}
